8ac0fd0e104053dde4b02510772c4bfff8698b19
vTiioma <asilantyeva@yahoo.com>
2017-12-31 16:31:10 -0500

    Initial Folder Setup
    Added initial folders with some basic utility scripts, scenes, and
    plugins
    
    Plugins
    -JSON.net
    -TextMesh Pro
    -LeanTween
    
    Folders
    -Editor
    -Resources
    -Scenes
    -Sprites
    
    Scripts
    -Keyboard Inputs
    -Touch Inputs
    -Mouse Inputs

---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T> {
+
+	private static T reference;
+	public static T instance {
+		get {
+			return reference;
+		}
+	}
+
+	public static bool isInitialized {
+		get {
+			return reference != null;
+		}
+	}
+
+	protected virtual void Awake () {
+		if (isInitialized) {
+			Debug.LogErrorFormat ("Trying to instantiate a second instance of singleton class {0}!", GetType ().Name);
+		} else {
+			reference = (T)this;
+		}
+	}
+
+	protected virtual void OnDestroy () {
+		if (reference == this) {
+			reference = null;
+		}
+	}
+}
