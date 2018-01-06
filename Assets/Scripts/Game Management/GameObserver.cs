6113168d83621d50d10ccf1200ec54b400183336
Artem <asilantyeva@yahoo.com>
2018-01-06 18:34:38 -0500

    added an observer for restart, pause, and game over also removed check for consistent line endings

---
+++ Assets/Scripts/Game Management/GameObserver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class GameObserver {
+
+    public static event Action onGameOver;
+    public static event Action onGamePaused;
+    public static event Action onGameRestart;
+    
+
+    public static void OnGameOver() {
+        if (onGameOver != null) {
+            onGameOver();
+        }
+    }
+
+    public static void OnGamePaused() {
+        if (onGamePaused != null) {
+            onGamePaused();
+        }
+    }
+
+    public static void OnGameRestart() {
+        if (onGameRestart != null) {
+            onGameRestart();
+        }
+    }
+}
