ea4503e4ad3b456539fde8d31f450b6e3bbe9f4c
Anubis523 <anubis523@gmail.com>
2018-01-06 19:30:25 -0500

    PreLoader_KDS
    Built preloader for start of game that on Start loads first level/scene.

---
+++ Assets/Scripts/PreLoader/PreLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PreLoader : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(LoadLevelAsync(2));
+        this.gameObject.SetActive(false);
+    }
+    /// <summary>
+    /// Function that calls the coroutine that runs the scene loader
+    /// </summary>
+    /// <param name="sceneIndex"></param>
+    public void LoadLevel(int sceneIndex)
+    {
+
+    }
+
+    /// <summary>
+    /// Loads level asynchronously
+    /// </summary>
+    /// <param name="sceneIndex"></param>
+    /// <returns></returns>
+    IEnumerator LoadLevelAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+            yield return null;
+        }
+
+    }
+}
